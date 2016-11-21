633a0c18f0147bd5e15f0e2dd375a1ab8b51c989
Gabriel Doucet <gabriel.doucet@eleve.enac.fr>
2016-11-21 23:37:30 +0000

    Using angular ui router

--- app.js
+++ app.js
@@ -1,72 +1,41 @@
-var am2App = angular.module('am2App', []);
+var am2App = angular.module('am2App', ['ui.router']);
 
-am2App.controller('AM2AppController', ['$scope', '$compile',
-  function AM2AppController($scope, $compile) {
-    $scope.coeffs = {eco: 1, business: 1.8, first: 4.23};
+am2App.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
 
-    $scope.optimize = function () {
-      $scope.answers = [];
-      _.forEach($scope.avions, function (avion) {
-        var demand = $scope.eco + $scope.business + $scope.first;
-        var pEco = $scope.eco / demand;
-        var pBusiness = $scope.business / demand;
-        var pFirst = $scope.first / demand;
-        var optiSeats = _.get(avion, 'sieges') / (pEco + pBusiness * $scope.coeffs.business + pFirst * $scope.coeffs.first);
-        var optiEco = _.round(pEco * optiSeats);
-        var optiBusiness = _.round(pBusiness * optiSeats);
-        var optiFirst = _.round(pFirst * optiSeats);
-        $scope.answers.push({type: avion.type, optiEco: optiEco, optiBusiness: optiBusiness, optiFirst: optiFirst});
-      })
-    };
+$urlRouterProvider.otherwise("/accueil");
 
-    $scope.avions = [
-      {
-        "type": "A320-NEO",
-        "sieges": 195,
-        "rayon": 6482,
-        "conso": 1.88,
-        "vitesse": 839,
-        "tonnage": 19.65
-      },
-      {
-        "type": "A380-800",
-        "sieges": 853,
-        "rayon": 15556,
-        "conso": 2.44,
-        "vitesse": 903,
-        "tonnage": 89.2
-      },
-      {
-        "type": "A350-900XWB",
-        "sieges": 475,
-        "rayon": 15186,
-        "conso": 2.11,
-        "vitesse": 911,
-        "tonnage": 47.5
-      },
-      { 
-        "type": "777-300ER",
-        "sieges": 550,
-        "rayon": 14695,
-        "conso": 2.28,
-        "vitesse": 892,
-        "tonnage": 69.9
-      },
-      {
-        "type": "777-200LR",
-        "sieges": 440,
-        "rayon": 17512,
-        "conso": 2.65,
-        "vitesse": 892,
-        "tonnage": 64
-      },
-      {
-        "type": "787-9",
-        "sieges": 420,
-        "rayon": 15556,
-        "conso": 2.12,
-        "vitesse": 911,
-        "tonnage": 42
+  $stateProvider
+    .state('root', {
+      abstract: true,
+      views: {
+        'header': {
+          templateUrl: 'layout/header.html'
+        }
       }
-    ];
+    })
+    .state('root.accueil', {
+      url: '/accueil',
+      views: {
+        '@': {
+           templateUrl: 'views/accueil.html'
+        }
+      }
+    })
+    .state('root.calculs', {
+      url: '/calculs',
+      views: {
+        '@': {
+          templateUrl: 'views/calculs.html',
+          controller: 'calculsController'
+        }
+      }
+    })
+    .state('root.flotte', {
+      url: '/flotte',
+      views: {
+        '@': {
+          templateUrl: 'views/flotte.html'
+        }
+      }
+    })
 }]);
---
+++ views/calculs.js
@@ -0,0 +1,71 @@
+var am2App = angular.module('am2App');
+
+am2App.controller('calculsController', ['$scope', function calculsController($scope) {
+    $scope.coeffs = {eco: 1, business: 1.8, first: 4.23};
+
+    $scope.optimize = function () {
+      $scope.answers = [];
+      _.forEach($scope.avions, function (avion) {
+        var demand = $scope.eco + $scope.business + $scope.first;
+        var pEco = $scope.eco / demand;
+        var pBusiness = $scope.business / demand;
+        var pFirst = $scope.first / demand;
+        var optiSeats = _.get(avion, 'sieges') / (pEco + pBusiness * $scope.coeffs.business + pFirst * $scope.coeffs.first);
+        var optiEco = _.round(pEco * optiSeats);
+        var optiBusiness = _.round(pBusiness * optiSeats);
+        var optiFirst = _.round(pFirst * optiSeats);
+        $scope.answers.push({type: avion.type, optiEco: optiEco, optiBusiness: optiBusiness, optiFirst: optiFirst});
+      })
+    };
+
+    $scope.avions = [
+      {
+        "type": "A320-NEO",
+        "sieges": 195,
+        "rayon": 6482,
+        "conso": 1.88,
+        "vitesse": 839,
+        "tonnage": 19.65
+      },
+      {
+        "type": "A380-800",
+        "sieges": 853,
+        "rayon": 15556,
+        "conso": 2.44,
+        "vitesse": 903,
+        "tonnage": 89.2
+      },
+      {
+        "type": "A350-900XWB",
+        "sieges": 475,
+        "rayon": 15186,
+        "conso": 2.11,
+        "vitesse": 911,
+        "tonnage": 47.5
+      },
+      { 
+        "type": "777-300ER",
+        "sieges": 550,
+        "rayon": 14695,
+        "conso": 2.28,
+        "vitesse": 892,
+        "tonnage": 69.9
+      },
+      {
+        "type": "777-200LR",
+        "sieges": 440,
+        "rayon": 17512,
+        "conso": 2.65,
+        "vitesse": 892,
+        "tonnage": 64
+      },
+      {
+        "type": "787-9",
+        "sieges": 420,
+        "rayon": 15556,
+        "conso": 2.12,
+        "vitesse": 911,
+        "tonnage": 42
+      }
+    ];
+}]);
