d86c95a4f3dbbd152ff00c7a167cb49fc3f4c277
Gabriel Doucet <gabriel_doucet@hotmail.fr>
2017-04-19 17:25:57 +0100

    Changing gulpfile for MDL version

--- gulpfile.js
+++ gulpfile.js
@@ -4,32 +4,9 @@ const browserify = require('browserify');
 const source     = require('vinyl-source-stream');
 const pug        = require('gulp-pug');
 
-// 'dist' task for the first version of the application
-gulp.task('browserify', function () {
-  return browserify('./app/app.js')
-    .bundle()
-    .pipe(source('main.js'))
-    .pipe(gulp.dest('./dist/js/'));
-});
-
-gulp.task('sass', function () {
-  return sass('./stylesheets/style.scss')
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./dist/stylesheets'));
-});
-
-gulp.task('watch', function () {
-  gulp.watch('app/**/*.js', ['browserify']);
-  gulp.watch('stylesheets/**/*.scss', ['sass']);
-});
-
-gulp.task('fonts', function () {
-  return gulp.src(['./node_modules/bootstrap-sass/assets/fonts/**/*'])
-    .pipe(gulp.dest('dist/fonts/'));
-});
 
 // 'dist-mdl' task for the mdl version of the application
-gulp.task('browserify-mdl', function () {
+gulp.task('browserify', function () {
   return browserify('./app/app-mdl.js')
     .bundle()
     .pipe(source('main-mdl.js'))
@@ -42,17 +19,16 @@ gulp.task('views', function buildHTML() {
     .pipe(gulp.dest('dist/views/')); // tell gulp our output folder
 });
 
-gulp.task('sass-mdl', function () {
+gulp.task('sass', function () {
   return sass('./stylesheets/style-mdl.scss')
     .on('error', sass.logError)
     .pipe(gulp.dest('./dist/stylesheets'));
 });
 
-gulp.task('watch-mdl', function () {
+gulp.task('watch', function () {
   gulp.watch('app/**/*.js', ['browserify-mdl']);
   gulp.watch('stylesheets/**/*.scss', ['sass-mdl']);
 });
 
 
-gulp.task('dist', ['browserify', 'sass', 'fonts', 'views', 'watch']);
-gulp.task('dist-mdl', ['browserify-mdl', 'views', 'sass-mdl', 'watch-mdl']);
+gulp.task('dist', ['browserify', 'views', 'sass', 'watch']);
