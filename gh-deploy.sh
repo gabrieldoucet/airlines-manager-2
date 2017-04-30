4d602e374480499b0cd929b233f2396d9fdbd602
Gabriel Doucet <gabriel_doucet@hotmail.fr>
2017-04-30 12:46:07 +0100

    New deploy script for github

--- gh-deploy.sh
+++ gh-deploy.sh
@@ -2,7 +2,9 @@
 
 if [ -z "$1" ]
 then
-  echo "Which folder do you want to deploy to GitHub Pages?"
+  echo "Which branch do you want to deploy the 'dist' folder from?"
   exit 1
 fi
-git subtree push --prefix $1 origin gh-pages
+git checkout gh-pages
+git pull origin $1
+git subtree push --prefix dist origin gh-pages
